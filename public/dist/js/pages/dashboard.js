3082be7f2a37a65529543280b619c02949b834e4
matieckhardt <matias.eckhardt@gmail.com>
2023-01-24 11:54:14 -0300

    dash datepicker limiter

--- public/dist/js/pages/dashboard.js
+++ public/dist/js/pages/dashboard.js
@@ -30,22 +30,31 @@ const changeDate = () => {
   console.log("redirected to /api/dashboard/" + split[0] + "/" + split[1]);
 };
 
+let today = new Date().toLocaleString("sv-SE", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
+console.log(today);
+
+document.getElementById("date").setAttribute("max", today);
+
 // ESTE MES
 
 const year = window.location.href.split("/").slice(5)[0];
 const month = window.location.href.split("/").slice(5)[1];
 const yearMonth = new Date(year + "/" + month);
 
-let today = yearMonth.toLocaleString("es-ar", {
+let añoMes = yearMonth.toLocaleString("es-ar", {
   year: "numeric",
   month: "long",
 });
 
 const setMes =
-  (document.getElementById("mes").innerText = today) &&
-  (document.getElementById("mes2").innerText = today) &&
-  (document.getElementById("yearMonth").innerText = today) &&
-  (document.getElementById("mesCompras").innerText = today);
+  (document.getElementById("mes").innerText = añoMes) &&
+  (document.getElementById("mes2").innerText = añoMes) &&
+  (document.getElementById("yearMonth").innerText = añoMes) &&
+  (document.getElementById("mesCompras").innerText = añoMes);
 
 // ESTE Año
 

eceb3736c7571a68850f5723024ea8d3b2b02576
Matias Eckhardt <info@switchit.com.ar>
2023-10-03 11:37:45 -0300

    grid 3 js

--- public/dist/js/pages/listaPrecios.js
+++ public/dist/js/pages/listaPrecios.js
@@ -1,14 +1,18 @@
-const elem = document.getElementById('grid');
-const msnry = new Masonry( elem, {
+const elem = document.getElementById("grid");
+const msnry = new Masonry(elem, {
   // options
-  itemSelector: '.grid-item',
-  columnWidth: 500
+  itemSelector: ".grid-item",
+  columnWidth: 500,
 });
-const elem2 = document.getElementById('grid2');
-const msnry2 = new Masonry( elem2, {
+const elem2 = document.getElementById("grid2");
+const msnry2 = new Masonry(elem2, {
   // options
-  itemSelector: '.grid-item',
-  columnWidth: 500
+  itemSelector: ".grid-item",
+  columnWidth: 500,
 });
-
-
+const elem3 = document.getElementById("grid3");
+const msnry3 = new Masonry(elem3, {
+  // options
+  itemSelector: ".grid-item",
+  columnWidth: 500,
+});
